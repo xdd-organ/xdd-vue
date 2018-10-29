e87d87c0cbefe0617f73325a75735311b6e561ba
xdd <767403510@qq.com>
2018-10-29 17:13:39 +0800

    指定端口启动项目

--- vue.config.js
+++ vue.config.js
@@ -33,9 +33,15 @@ module.exports = {
             .set('_conf', resolve('config'))
     },
     // 打包时不生成.map文件
-    productionSourceMap: false
+    productionSourceMap: false,
     // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
-    // devServer: {
-    //   proxy: 'https://www.chmbkh.com/'
-    // }
+    devServer: {
+        // open: process.platform === 'darwin',
+        // host: '0.0.0.0',
+        port: 8088
+        // https: false,
+        // hotOnly: false,
+        // proxy: null, // 设置代理
+        // before: app => {}
+    }
 }
